fc125b2469093b03e59e9a3724c007a66b6fa135
srbvida <srbvida@mail.com>
2025-01-12 16:25:24 +0100

    Added security

--- src/main/java/com/example/baseback/controllers/DAWController.java
+++ src/main/java/com/example/baseback/controllers/DAWController.java
@@ -1,11 +1,16 @@
 package com.example.baseback.controllers;
 
 import com.example.baseback.repository.ProductRepository;
+import com.example.baseback.security.JwtUtil;
+import com.example.baseback.security.UserService;
 import com.example.baseback.service.ProductEntity;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,15 +23,24 @@ import java.util.Optional;
 @SuppressWarnings({"java:S5738", "java:S4684"})
 class DAWController {
     private ProductRepository repository;
+    private JwtUtil jwtUtil;
+    private UserDetailsService userDetailsService;
+    private UserService userService;
 
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest request) {
         log.debug("A request has arrived to login");
-        if ("user".equals(request.getUsername()) && "password".equals(request.getPassword())) {
-            return ResponseEntity.ok(new LoginResponse("Bearer dummy-token"));
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+        try{
+            UserDetails userDetails = userDetailsService.loadUserByUsername(request.getUsername());
+            // Validar la contraseña
+            if (userService.validatePassword(request.getPassword(), userDetails.getPassword())) {
+                return ResponseEntity.ok(new LoginResponse(jwtUtil.generateToken(userDetails.getUsername())));
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuario o contraseña incorrectos");
+            }
+        }catch (UsernameNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuario no encontrado");
         }
     }
 
